27ff57c18b0584aacfcf28f44cef7210f367bc32
Juan <malachias322@gmail.com>
2023-09-01 10:29:47 -0300

    Criar o Projeto Spring

---
+++ src/main/java/com/generation/projeto_MaternaCare/ProjetoMaternaCareApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projeto_MaternaCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMaternaCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMaternaCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/projeto_MaternaCare/ProjetoMaternaCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projeto_MaternaCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMaternaCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
